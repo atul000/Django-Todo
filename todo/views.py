e94a98e3e03d5d58b08a5e6976a466fee51b3a57
atul <atulbeniwal15@gmail.com>
2020-03-26 21:13:09 +0530

    version 3

--- todo/views.py
+++ todo/views.py
@@ -70,7 +70,7 @@ def createtodo(request):
 
 
 def viewtodo(request, todo_pk):
-    todo = get_object_or_404(Todo, pk=todo_pk)
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
     if request.method == 'GET':
         form = TodoForm(instance=todo)
         return render(request, 'todo/viewtodo.html', {'todo': todo, 'form': form})
